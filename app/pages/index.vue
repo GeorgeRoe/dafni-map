98bd98c23db3eec2e75299bfcb1da27f1ac22b35
George Roe <george.roe@stfc.ac.uk>
2026-06-19 14:28:37 +0100

    feat: add slideover

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
 import partnerPointData from '~/assets/data/partnerPoints.json'
 import { useValidatedPoints } from '~/composables/useValidatedPoints'
-import type { MapGeoJSONFeature } from 'maplibre-gl'
 import type { Partner } from '~/types/partner'
 
 const partnerPoints = useValidatedPoints(partnerPointData)
 
+const slideoverOpen = ref(false)
+const activePartner = ref<Partner | null>(null)
+
 function onClick(partner: Partner) {
-  console.log('Feature Clicked:', partner)
+  activePartner.value = partner
+  slideoverOpen.value = true
 }
 </script>
 
 <template>
   <div class="h-[calc(100vh-var(--ui-header-height))]">
+    <USlideover
+      v-model:open="slideoverOpen"
+      :modal="false"
+      side="left"
+      inset
+      :ui="{
+        content: 'top-[calc(1rem+var(--ui-header-height))]'
+      }"
+    >
+      <template #content>
+        <UButton
+          color="neutral"
+          variant="subtle"
+          class="mb-4 w-fit m-2"
+          icon="i-lucide-arrow-left"
+          @click="slideoverOpen = false"
+        />
+        <pre>{{ activePartner }}</pre>
+      </template>
+    </USlideover>
     <Map
       @click="onClick"
       :partner-points="partnerPoints"
